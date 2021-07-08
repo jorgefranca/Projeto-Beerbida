d60be197495fd6932ca88e2bafb0b19c3feea843
jorgefranca <jorgeartur.f@gmail.com>
2021-07-08 15:34:38 -0300

    primeira etapa do cadastro de cerveja

--- src/main/java/com/projeto/beerbida/controller/CervejasController.java
+++ src/main/java/com/projeto/beerbida/controller/CervejasController.java
@@ -2,6 +2,9 @@ package com.projeto.beerbida.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.projeto.beerbida.model.Cerveja;
 
 @Controller
 public class CervejasController {
@@ -11,6 +14,16 @@ public class CervejasController {
 	@RequestMapping("/cervejas/novo")
 	public String novo(){
 		return "cerveja/CadastroCerveja";
+	}
+	//o spring já cria o objeto com os parâmetros que queremos.
+	//por isso é importante ter o construtor padrão
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(Cerveja cerveja){
+		System.out.println("Cerveja cadastrada com sucesso!");
+		System.out.println("SKU: " + cerveja.getSku());
+		System.out.println("NOME: " + cerveja.getNome());
+		return "cerveja/CadastroCerveja";
+		
 	}
 
 }
---
+++ src/main/java/com/projeto/beerbida/model/Cerveja.java
@@ -0,0 +1,24 @@
+package com.projeto.beerbida.model;
+
+public class Cerveja {
+	//atributos
+	private String sku;
+	private String nome;
+	
+	
+	
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
