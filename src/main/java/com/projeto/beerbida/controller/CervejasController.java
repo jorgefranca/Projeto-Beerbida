7edb3da964167bb5425e5d35744578c4c8590e5c
jorgefranca <jorgeartur.f@gmail.com>
2021-08-07 00:20:05 -0300

    Forward e Redirect

--- src/main/java/com/projeto/beerbida/controller/CervejasController.java
+++ src/main/java/com/projeto/beerbida/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.projeto.beerbida.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.projeto.beerbida.model.Cerveja;
 
@@ -15,21 +17,26 @@ public class CervejasController {
 	 * vamos usar o RequestMapping()
 	 */
 	@RequestMapping("/cervejas/novo")
+	//forward: chega a requisição e já encaminha 
 	public String novo(){
 		return "cerveja/CadastroCerveja";
 	}
 	//o spring já cria o objeto com os parâmetros que queremos.
 	//por isso é importante ter o construtor padrão
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar( @Valid Cerveja cerveja, BindingResult result){
+	public String cadastrar( @Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes){
 		if(result.hasErrors()){
-			System.out.println(">>> TEM ERRO SIM!!");
-		}else{
-			System.out.println("Cerveja cadastrada com sucesso!");
-			System.out.println("SKU: " + cerveja.getSku());
-			System.out.println("NOME: " + cerveja.getNome());
+			model.addAttribute("mensagem", "Erro no formulário");
+			return "cerveja/CadastroCerveja";
+			// return ...
 		}
-		return "cerveja/CadastroCerveja";
+			// salvar no banco de dados...
+		attributes.addFlashAttribute("mensagem", "Cerveja cadastrada com sucesso!");
+		System.out.println("Cerveja cadastrada com sucesso!");
+		System.out.println("SKU: " + cerveja.getSku());
+		System.out.println("NOME: " + cerveja.getNome());	
+		
+		return "redirect:/cervejas/novo";
 		
 	}
 
