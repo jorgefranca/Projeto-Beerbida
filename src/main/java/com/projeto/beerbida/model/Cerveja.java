0a67db97353a39ef48bf4fe0483e87c1187b8cef
jorgefranca <jorgeartur.f@gmail.com>
2021-08-07 01:44:29 -0300

    adicionado campo de descrição

--- src/main/java/com/projeto/beerbida/model/Cerveja.java
+++ src/main/java/com/projeto/beerbida/model/Cerveja.java
@@ -1,13 +1,15 @@
 package com.projeto.beerbida.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
-	
 	@NotBlank //verifica se o campo é nulo (retira espaços)
 	private String sku;
 	private String nome;
-	
+	@Size(max=50)@NotBlank
+	private String descricao;
 	
 	
 	public String getSku() {
@@ -21,6 +23,12 @@ public class Cerveja {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getDescricao(){
+		return descricao;
+	}
+	public void setDescricao(String descricao){
+		this.descricao = descricao;
 	}
 	
 	
